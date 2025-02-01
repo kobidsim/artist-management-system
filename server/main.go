d5f1f9fc29860e5155e66ae07bc4fd3b7ac3374a
kobidsim <simkhadakobid@gmail.com>
2025-02-01 14:40:11 +0545

    initial commit

---
+++ server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	app := echo.New()
+
+	app.GET("/", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "Hello World")
+	})
+
+	app.Logger.Fatal(app.Start(":8080"))
+}
